82d87bf2c291a2dd3848cfb3a7e5bbe3627bf578
nazmul128 <shafayat536@gmail.com>
2021-12-12 13:57:39 +0600

    code addedd

--- src/Pages/Home/Css/Css.js
+++ src/Pages/Home/Css/Css.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+
 
 const Css = () => {
     return (
         <div>
             <h1>This is a CSS question</h1>
+            <Typography sx={{width:'50%', textAlign:'left'}} variant="body1" gutterBottom>
+           <strong style={{marginLeft:'10px'}}>1.  CSS stands for -</strong>
+           <ul style={{listStyleType:'circle'}}>
+               <li>Cascade style sheets</li>
+               <li>Color and style sheets</li>
+               <li>Cascading style sheets</li>
+               <li>None of these</li>
+           </ul>
+      </Typography>
+            <Typography sx={{width:'50%', textAlign:'left'}} variant="body1" gutterBottom>
+           <strong style={{marginLeft:'10px'}}>2) Which of the following is the correct syntax for referring the external style sheet?</strong>
+           <ul style={{listStyleType:'circle'}}>
+               <li>style src=example.css</li>
+               <li>style src = "example.css</li>
+               <li>stylesheet example.css stylesheet</li>
+               
+           </ul>
+      </Typography>
+
+
+            <Typography sx={{width:'50%', textAlign:'left'}} variant="body1" gutterBottom>
+           <strong style={{marginLeft:'10px'}}>3) The property in CSS used to change the background color of an element is -</strong>
+           <ul style={{listStyleType:'circle'}}>
+               <li>bgColour</li>
+               <li>colour</li>
+               <li>background-color</li>
+               <li>All of the above
+</li>
+           </ul>
+      </Typography>
+           
+           
+            
+
+          
         </div>
     );
 };
--- src/Pages/Home/Html/Html.js
+++ src/Pages/Home/Html/Html.js
@@ -1,9 +1,60 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+
 
 const Html = () => {
     return (
         <div>
-            <h1>This is a Html Question</h1>
+            <Typography sx={{width:'50%', textAlign:'left'}} variant="body1" gutterBottom>
+           <strong style={{marginLeft:'10px'}}>1. HTML stands for -</strong>
+           <ul style={{listStyleType:'circle'}}>
+               <li>HighText Machine Language</li>
+               <li>HyperText and links Markup Language</li>
+               <li>HyperText Markup Language</li>
+               <li>None of these</li>
+           </ul>
+      </Typography>
+            <Typography sx={{width:'50%', textAlign:'left'}} variant="body1" gutterBottom>
+           <strong style={{marginLeft:'10px'}}>2) Which of the following element is responsible for making the text bold in HTML?</strong>
+           <ul style={{listStyleType:'circle'}}>
+               <li>pre</li>
+               <li>a</li>
+               <li>b</li>
+               <li>br</li>
+           </ul>
+      </Typography>
+
+
+            <Typography sx={{width:'50%', textAlign:'left'}} variant="body1" gutterBottom>
+           <strong style={{marginLeft:'10px'}}>3) Which of the following tag is used for inserting the largest heading in HTML?</strong>
+           <ul style={{listStyleType:'circle'}}>
+               <li>h3</li>
+               <li>h1</li>
+               <li>h5</li>
+               <li>h6</li>
+           </ul>
+      </Typography>
+           
+           
+            <Typography sx={{width:'50%', textAlign:'left'}} variant="body1" gutterBottom>
+           <strong style={{marginLeft:'10px'}}>4) How to create an unordered list (a list with the list items in bullets) in HTML?</strong>
+           <ul style={{listStyleType:'circle'}}>
+               <li>ul</li>
+               <li>ol</li>
+               <li>li</li>
+               <li>i</li>
+           </ul>
+      </Typography>
+
+            <Typography sx={{width:'50%', textAlign:'left'}} variant="body1" gutterBottom>
+           <strong style={{marginLeft:'10px'}}>5) Which character is used to represent the closing of a tag in HTML?</strong>
+           <ul style={{listStyleType:'circle'}}>
+               <li>\</li>
+               <li>!</li>
+               <li>/</li>
+               <li>.</li>
+           </ul>
+      </Typography>
         </div>
     );
 };
